f92d1eaeae2b80dc228949617feb549325206c2e
hyeondoong2 <sallybi@naver.com>
2024-11-15 16:05:04 +0900

    메뉴 띄우기 성공~!

--- menu.py
+++ menu.py
@@ -4,20 +4,42 @@ class GameStart:
     def __init__(self):
         self.image = load_image('game_start_sprite_sheet.png')
         self.frame = 0
+        self.x, self.y = 370, 500  # 메뉴의 중심 좌표
+        self.width, self.height = 300, 100 # 메뉴의 크기
+        self.isDragged = False  # 마우스가 올라가 있는지 여부
 
     def draw(self):
-        self.image.clip_draw(self.frame * 300, 0, 100, 100, 400, 500)
+        self.image.clip_draw(self.frame * 300, 0, self.width, self.height, self.x, self.y)
 
-    def update(self):
-        pass
+    def update(self, mouse_x, mouse_y):
+        # 마우스가 메뉴 영역에 들어갔는지 확인
+        self.isDragged = (self.x - self.width // 2 <= mouse_x <= self.x + self.width // 2 and
+                      self.y - self.height // 2 <= mouse_y <= self.y + self.height // 2)
+
+        if self.isDragged == True:
+            self.frame = 1
+        else:
+            self.frame = 0
 
 class GameRule:
     def __init__(self):
         self.image = load_image('game_rule_sprite_sheet.png')
         self.frame = 0
+        self.x, self.y = 370, 370  # 메뉴의 중심 좌표
+        self.width, self.height = 300, 100  # 메뉴의 크기
+        self.isDragged = False  # 마우스가 올라가 있는지 여부
 
     def draw(self):
-        self.image.clip_draw(self.frame * 745, 0, 100, 100, 400, 300)
+        self.image.clip_draw(self.frame * 300, 0, self.width, self.height, self.x, self.y)
 
-    def update(self):
+    def update(self, mouse_x, mouse_y):
+        # 마우스가 메뉴 영역에 들어갔는지 확인
+        self.isDragged = (self.x - self.width // 2 <= mouse_x <= self.x + self.width // 2 and
+                          self.y - self.height // 2 <= mouse_y <= self.y + self.height // 2)
+
+        if self.isDragged == True:
+            self.frame = 1
+        else:
+            self.frame = 0
+
         pass
--- title_mode.py
+++ title_mode.py
@@ -1,9 +1,10 @@
 from pico2d import load_image, get_time, clear_canvas, update_canvas, get_events
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDL_MOUSEMOTION, SDL_MOUSEBUTTONDOWN, SDL_MOUSEBUTTONUP
-import game_framework, play_mode
+import game_framework, play_mode, rule_mode
 from menu import GameRule
 from menu import GameStart
 
+mouse_x, mouse_y = 0, 0
 
 def handle_events():
     global running
@@ -15,20 +16,22 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif  SDL_MOUSEMOTION:
-            mouse_x = event.x
-            mouse_y = event.y
-            #game_framework.change_mode(play_mode)
-        elif  SDL_MOUSEBUTTONUP:
-            mouse_x = event.x
-            mouse_y = event.y
-            #game_framework.change_mode(play_mode)
-
+        elif event.type == SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, 900 - event.y  # PyGame은 좌표계가 다름
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            # 메뉴 클릭 처리
+            if gameStart.isDragged:
+                game_framework.change_mode(play_mode)  # GameStart 메뉴 클릭 시 플레이 모드로 전환
+            elif gameRule.isDragged:
+                game_framework.change_mode(rule_mode)  # GameRule 메뉴 클릭 시 규칙 출력 (예시)
 
 def init():
     global image
-    global gamestart
-    global gamerule
+    global gameStart
+    global gameRule
+
+    gameStart = GameStart()
+    gameRule = GameRule()
 
     image = load_image('lobby_background.png')
 
@@ -39,11 +42,15 @@ def finish():
 
 
 def update():
-    pass
+    # 마우스 상태에 따라 메뉴 업데이트
+    gameStart.update(mouse_x, mouse_y)
+    gameRule.update(mouse_x, mouse_y)
 
 
 
 def draw():
     clear_canvas()
     image.draw(800, 450)
+    gameStart.draw()
+    gameRule.draw()
     update_canvas()

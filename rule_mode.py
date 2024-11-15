37b7e7232a344cb0844b5da4c6b841762262c6d2
hyeondoong2 <sallybi@naver.com>
2024-11-15 16:12:06 +0900

    rule_mode에도 게임시작 버튼 띄움

--- rule_mode.py
+++ rule_mode.py
@@ -1,10 +1,13 @@
 from pico2d import load_image, get_time, clear_canvas, update_canvas, get_events
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDL_MOUSEMOTION, SDL_MOUSEBUTTONDOWN, SDL_MOUSEBUTTONUP
 import game_framework, play_mode
+from menu import GameStart
 
+mouse_x, mouse_y = 0, 0
 
 def handle_events():
     global running
+    global mouse_x, mouse_y
 
     events = get_events()
     for event in events:
@@ -12,12 +15,21 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif  event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            game_framework.change_mode(play_mode)
+        elif event.type == SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, 900 - event.y  # PyGame은 좌표계가 다름
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            # 메뉴 클릭 처리
+            if gameStart.isDragged:
+                game_framework.change_mode(play_mode)  # GameStart 메뉴 클릭 시 플레이 모드로 전환
 
 
 def init():
     global image
+    global gameStart
+    gameStart = GameStart()
+
+    gameStart.x, gameStart.y = 1400, 170
+
     image = load_image('rule_background.png')
 
 
@@ -27,10 +39,13 @@ def finish():
 
 
 def update():
+    # 마우스 상태에 따라 메뉴 업데이트
+    gameStart.update(mouse_x, mouse_y)
     pass
 
 
 def draw():
     clear_canvas()
     image.draw(800, 450)
+    gameStart.draw()
     update_canvas()
